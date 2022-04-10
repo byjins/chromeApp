6ff5ed6bd336338c2fd463d3aba73fe2630481e8
Jinny <ppwm111@naver.com>
2022-04-10 17:08:20 +0900

    event

---
+++ app.js
@@ -0,0 +1,10 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  console.log(username);
+}
+
+loginForm.addEventListener('submit', onLoginSubmit);
